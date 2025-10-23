cdf927b2022ff1d1522e035e2deefa74bbd86963
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-22 23:42:03 -0500

    Update page.js

--- emeraldsdt-front/src/app/page.js
+++ emeraldsdt-front/src/app/page.js
@@ -48,7 +48,7 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
