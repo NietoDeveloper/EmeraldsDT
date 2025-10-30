f0c03ff62a4c025308b54fe7dab5c53e1abfec8f
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-10-30 18:29:00 -0500

    Update page.js

--- emeraldsdt-front/src/app/page.js
+++ emeraldsdt-front/src/app/page.js
@@ -47,48 +47,7 @@ export default function Home() {
         </div>
       </main>
       <footer className={styles.footer}>
-        <a
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Buy
-        </a>
-        <a
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          About →
-        </a>
+
       </footer>
     </div>
   );
