26e3e5f5358be01afbe280b7d957e8bf0d660032
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-01 18:53:11 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = () => {
                     htmlFor="first_name"
                     className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
-                    * First Name
+                    * Primer Nombre
                   </label>
                   <input
                     type="text"
