118c282025d3c59e32cbccbf7d540f5582a50124
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-30 20:43:25 -0500

    Update NavItem.jsx

--- emeraldsdt-front/src/components/NavItem.jsx
+++ emeraldsdt-front/src/components/NavItem.jsx
@@ -5,21 +5,7 @@ import { FaAngleDown } from "react-icons/fa6";
 function NavItem({ text, menu, link, pdf, setMenu, drop, index }) {
   const location = useLocation();
 
-  const isActive =
-    location.pathname === link && location.pathname !== "/our-menu/main";
-  const isActivemenu = location.pathname === "/our-menu/main";
 
-  return (
-    <li
-      className={`capitalize px-5 text-[18px]  h-full font-hobo-light relative transition-all duration-100 cursor-pointer after:lg:block after:hidden flex items-center lg:mt-0 mt-4 ${
-        isActive
-          ? "font-hobo-light font-[700] border-b-4 border-b-red-500 text-[18px]"
-          : ""
-      }`}
-      onClick={() => {
-        if (index === 0) {
-
-    </li>
   );
 }
 
