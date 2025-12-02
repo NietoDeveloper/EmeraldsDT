c5b1d3e712edb0c287d43f5756cc991bfa280bb1
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-01 19:35:03 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -85,20 +85,4 @@ const Contact = () => {
                   className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                 >
                   Country Code
-                </label>
-                <input
-                  type="text"
-                  name="country_code"
-                  id="country_code"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
-                  placeholder="e.g. 9XX"
-                />
-              </div>
-              <div className="relative z-0 w-full mb-5 group">
-                <label
-                  htmlFor="mobile_number"
-                  className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Mobile Number
-                </label>
-                <input
+
