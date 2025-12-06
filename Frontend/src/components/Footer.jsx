731948c42e1964947293191ce831109585bd7d4e
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-05 22:31:24 -0500

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -20,16 +20,4 @@ const Footer = () => {
   return (
     <div className="w-full lg:pl-10 px-7 lg:pr-5 mt-10 ">
       <hr className="mb-10" />
-      <div className="w-full flex justify-between gap-7 max-sm:flex-col">
-        <div>
 
-        <div>
-
-          >
-            <FaFacebookF className="text-md" />
-          </a>
-        </div>
-        <div className="h-10 w-10 rounded-full border-[1.5px] border-black flex items-center justify-center cursor-pointer hover:bg-white  transition-all duration-300">
-          <a
-            href="https://www.instagram.com/letsfalafelofficial/"
-            target="_blank"
