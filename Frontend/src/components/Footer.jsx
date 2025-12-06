3f249a551a085bdeebb2838f9ef4cd7651111147
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-05 22:31:42 -0500

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,13 +3,5 @@ import {
   FaFacebookF,
   FaInstagram,
   FaLinkedinIn,
-  FaTwitter,
-} from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
 
-  };
-
-  return (
-    <div className="w-full lg:pl-10 px-7 lg:pr-5 mt-10 ">
-      <hr className="mb-10" />
 
