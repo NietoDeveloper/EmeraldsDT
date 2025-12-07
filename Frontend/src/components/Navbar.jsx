f0fd5f5f763233620deb2f9a0893da0d20cb0467
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-12-06 20:10:12 -0500

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -198,7 +198,7 @@ const Navbar = () => {
             onClick={handleviewfullmenu}
             className="bg-color-red text-white px-3 py-2 rounded-md font-hobo"
           >
-            View Full Menu
+            View Full Products
           </button>
         </div>
         <div className="grid lg:grid-cols-3 content-center gap-3 grid-cols-1 pt-5  lg:w-[60%] w-full">
