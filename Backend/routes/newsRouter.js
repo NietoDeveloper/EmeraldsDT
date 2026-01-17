930f008451c3e082f00597dd21ccbb7d4243f1a2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 00:03:08 -0500

    Update newsRouter.js

--- Backend/routes/newsRouter.js
+++ Backend/routes/newsRouter.js
@@ -4,25 +4,7 @@ import {
   getNews,
   getNewsById,
   updateNews,
-  deleteNews
-} from "../controllers/newsController.js";
 
-const Router = express.Router();
-
-Router.get("/", (req, res) => {
-  res.send("News");
-});
-
-// create
-Router.post("/createNews", createNews);
-
-// read
-Router.get("/readNews", getNews);
-
-// get news by id
-Router.get("/getnews/:id", getNewsById);
-
-// update
 Router.post("/updateNews/:id", updateNews);
 
 // delete
