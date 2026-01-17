cc518d64149bf9f56529d5b580718e3bd5bd46a9
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:17:59 -0500

    Update corsOption.js

--- Backend/middlewares/corsOption.js
+++ Backend/middlewares/corsOption.js
@@ -3,12 +3,3 @@ import allowedOrigins from "./allowedOrigins.js";
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("not allowed by cors"));
-    }
-  },
-  operationsSuccessStatus: 200,
-};
-
-export default corsOptions;
