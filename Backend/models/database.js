9cb8bb4255e9d09ec9eea455baffbc9d573887dc
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:27:09 -0500

    Update database.js

--- Backend/models/database.js
+++ Backend/models/database.js
@@ -1,17 +1,5 @@
 import mongoose from "mongoose";
 import Admin from "./AdminModel.js";
 
-const Connection = async () => {
-  try {
-    await mongoose.connect(
-      "mongodb+srv://kuldeepkaware77:K2mzg4IrzGedxwzv@cluster0.lsdtocv.mongodb.net/letsfalafel?retryWrites=true&w=majority"
-    );
-    console.log("MongoDB Connected");
-    initial();
-  } catch (error) {
-    console.log(error, "Error while connecting to database");
-  }
-("Admin already created");
-  }
 };
 export default Connection;
