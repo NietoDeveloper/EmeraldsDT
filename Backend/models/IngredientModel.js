e3ea54de44ef444ed45f739f28bd35c7e85cab36
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:27:36 -0500

    Update IngredientModel.js

--- Backend/models/IngredientModel.js
+++ Backend/models/IngredientModel.js
@@ -1,15 +1,5 @@
 import mongoose from "mongoose";
 let { ObjectId } = mongoose.Types;
 
-const ingredientSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-
-  },
-  { timestamps: true }
-);
 
 export default mongoose.model("Ingredient", ingredientSchema);
