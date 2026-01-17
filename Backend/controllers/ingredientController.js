68a2f6ede0284653270af8559f3eca359ae37369
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:02:59 -0500

    Update ingredientController.js

--- Backend/controllers/ingredientController.js
+++ Backend/controllers/ingredientController.js
@@ -2,13 +2,3 @@ import Ingredient from "../models/IngredientModel.js";
 import { initImageKit } from "../utils/imageKit.js";
 import path from "path";
 import Item from "../models/ItemModel.js";
-const imagekit = initImageKit();
-
-
-    return res.status(200).json({
-      success: true,
-      message: "Ingredient fetched successfully",
-      ingredient,
-    });
-  } catch (error) {
-;
