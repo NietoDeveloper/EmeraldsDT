cbeb1efec83be3d2389db0e9c1a340a860824b81
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 00:16:06 -0500

    Update ingredientRouter.js

--- Backend/routes/ingredientRouter.js
+++ Backend/routes/ingredientRouter.js
@@ -1,17 +1,2 @@
 import express from "express";
 import {
-  createIngredient,
-  deleteIngredient,
-  getIngredientById,
-  readAllIngredients,
-  updateIngredient,
-} from "../controllers/ingredientController.js";
-
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("Ingredient Router");
-});
-
-
-export default router;
