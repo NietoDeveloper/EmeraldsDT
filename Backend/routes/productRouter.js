3f1584d09f20db99cfac617dfdcb058410db8fba
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:48:16 -0500

    Update productRouter.js

--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -7,25 +7,6 @@ import {
   getProductById
 } from "../controllers/productController.js";
 
-const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Product Router");
-});
-
-// create
-router.post("/createProduct", createProduct);
-
-// read
-router.get("/getProducts", getProduct);
-
-// read single product
-router.get("/getProduct/:id", getProductById);
-
-// update
-router.post("/updateProduct/:id", updateProduct);
-
-// delete
-router.delete("/deleteProduct/:id", deleteProduct);
 
 export default router;
