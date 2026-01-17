36c4ede5c1fb4f7d0b9fe4307708637fbf278304
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 22:28:11 -0500

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -27,9 +27,7 @@ export const allUsers = async (req, res) => {
   try {
     const users = await User.find({});
     res.status(200).json({
-    
-      message: "All Users",
-      users: users,
+ 
     });
   } catch (error) {
     console.log(error);
