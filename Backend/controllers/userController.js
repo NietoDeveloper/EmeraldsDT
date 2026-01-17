e2c9f3fc07abbe3d88d9eee2653a46ff32964754
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 22:29:18 -0500

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -16,7 +16,7 @@ export const UserById = async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Internal Server Error" });
+    
   }
 };
 
