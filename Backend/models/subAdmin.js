00a2bf10dfb2e6dfc220c38abb4d93bce2012df3
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:43:33 -0500

    Update subAdmin.js

--- Backend/models/subAdmin.js
+++ Backend/models/subAdmin.js
@@ -1,16 +1,1 @@
-import mongoose from "mongoose";
 
-const subAdminSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-
-});
-
-const SubAdmin = mongoose.model("SubAdmin", subAdminSchema);
-
-export default SubAdmin;
