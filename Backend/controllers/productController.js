069f0014b12da0ecac21bf45bb3fded69a8ab134
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 22:57:23 -0500

    Update productController.js

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,6 +1,2 @@
-import Product from "../models/ProductModel.js";
-import Item from "../models/ItemModel.js";
-import Category from "../models/Category.js";
-t fileName = `falafel-${Date.now()}${path.extname(file.name)}`;
 
 };
