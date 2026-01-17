8a8f40395bf46f08db2b3e3bc9beeb45d96fe230
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 22:02:23 -0500

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -15,12 +15,7 @@ export const userRegister = async (req, res) => {
       return res.status(400).json({ error: "User already exists" });
   
 
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
+
 
 export const userLogin = async (req, res) => {
   try {
