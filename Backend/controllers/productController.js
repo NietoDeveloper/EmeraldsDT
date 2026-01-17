ba95e4ddf046365c8e17f4e5e59b8b73d65f6a01
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 22:57:13 -0500

    Update productController.js

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -4,11 +4,6 @@ import Category from "../models/Category.js";
 import path from "path";
 import { initImageKit } from "../utils/imageKit.js";
 const imagekit = initImageKit();
-
-export const createProduct = async (req, res) => {
-  try {
-    const file = req.files.image;
-    const { name, price, stock, description } = req.body;
-    const fileName = `falafel-${Date.now()}${path.extname(file.name)}`;
+onst fileName = `falafel-${Date.now()}${path.extname(file.name)}`;
 
 };
