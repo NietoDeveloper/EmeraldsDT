8b14c1f3f423e064a393e13c71a88468dc1929ea
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 00:03:14 -0500

    Update newsRouter.js

--- Backend/routes/newsRouter.js
+++ Backend/routes/newsRouter.js
@@ -1,13 +1,1 @@
-import express from "express";
-import {
-  createNews,
-  getNews,
-  getNewsById,
-  updateNews,
 
-Router.post("/updateNews/:id", updateNews);
-
-// delete
-Router.delete("/deleteNews/:id", deleteNews)
-
-export default Router;
