4c3b3f55f2b198f7efddec1e5481f61519547b9e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 00:15:59 -0500

    Update ingredientRouter.js

--- Backend/routes/ingredientRouter.js
+++ Backend/routes/ingredientRouter.js
@@ -13,14 +13,5 @@ router.get("/", (req, res) => {
   res.send("Ingredient Router");
 });
 
-router.post("/create", createIngredient);
-
-router.get("/read/all", readAllIngredients);
-
-router.post("/update/:id", updateIngredient);
-
-router.delete("/delete/:id", deleteIngredient);
-
-router.get("/getById/:id", getIngredientById);
 
 export default router;
