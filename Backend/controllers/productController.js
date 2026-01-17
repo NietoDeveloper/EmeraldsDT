73c35120ee718941930e0002ce81aabd0544db76
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 22:57:18 -0500

    Update productController.js

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,9 +1,6 @@
 import Product from "../models/ProductModel.js";
 import Item from "../models/ItemModel.js";
 import Category from "../models/Category.js";
-import path from "path";
-import { initImageKit } from "../utils/imageKit.js";
-const imagekit = initImageKit();
-onst fileName = `falafel-${Date.now()}${path.extname(file.name)}`;
+t fileName = `falafel-${Date.now()}${path.extname(file.name)}`;
 
 };
