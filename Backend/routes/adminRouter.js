82ecc3011b017b1273d1ba7f06570697977efa39
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 00:22:09 -0500

    Update adminRouter.js

--- Backend/routes/adminRouter.js
+++ Backend/routes/adminRouter.js
@@ -12,23 +12,3 @@ import {
 
 const router = express.Router();
 
-// initial route for admin
-router.get("/", (req, res) => {
-  res.send("Admin Router");
-});
-
-
-// routes for banner
-router.post("/addBanner", addBanner);
-router.get("/getBanner", getBanner);
-router.post("/deleteBanner/:bannerId", deleteBanner);
-
-
-// category route
-router.post("/addCategory", addCategory);
-router.get("/getCategory", getCategory);
-router.get("/getCategoryById/:categoryId", getCategoryById);
-router.delete("/deleteCategory/:categoryId", deleteCategory);
-router.post("/updateCategory/:categoryId", updateCategory);
-
-export default router;
