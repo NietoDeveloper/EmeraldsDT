d4e34037e7eb2b065e804aeebd0e6ffaf5f2e75c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:45:35 -0500

    Update ProductModel.js

--- Backend/models/ProductModel.js
+++ Backend/models/ProductModel.js
@@ -23,10 +23,7 @@ const productSchema = new mongoose.Schema(
         type: String,
         required: true,
       },
-      url: {
-        type: String,
-        required: true,
-      },
+
     },
   },
   {
