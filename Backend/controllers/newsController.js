103c9545f5a24f0bb751fe2c34df89e23d9a36be
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:00:30 -0500

    Update newsController.js

--- Backend/controllers/newsController.js
+++ Backend/controllers/newsController.js
@@ -3,12 +3,3 @@ import { initImageKit } from "../utils/imageKit.js";
 import path from "path";
 const imagekit = initImageKit();
 
-
-    if (req.files) {
-      const file = req.files.image;
-      const fileName = `falafel-news-${Date.now()}${path.extname(file.name)}`;
-
-  } catch (error) {
-    res.status(500).json({ message: error.message, success: false });
-  }
-};
