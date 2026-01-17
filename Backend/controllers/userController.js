136bcfe86fc99a7aede303a8fc4994a193a94847
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 22:04:40 -0500

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -17,14 +17,8 @@ export const userRegister = async (req, res) => {
 export const userLogin = async (req, res) => {
   try {
 
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ error: "Invalid credentials" });
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: "Invalid credentials" });
-    }
+   
+  
     const token = jwt.sign({ _id: user._id }, "SECRETTOKEN");
     res.cookie("token", token, {
       httpOnly: true,
