7a24ab04fd0bbfa60efff8ac029ca836eec4f102
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-17 00:47:15 -0500

    Update imageKit.js

--- Backend/utils/imageKit.js
+++ Backend/utils/imageKit.js
@@ -2,11 +2,7 @@
 import ImageKit from "imagekit";
 
 const initImageKit = function () {
-  var imagekit = new ImageKit({
-    publicKey: "public_bejh3MK0Sa4+/NI9wmdBfERbyYU=",
-    privateKey: "private_6rvTsbRGR7xnYrmZtFpud23XBk0=",
-    urlEndpoint: "https://ik.imagekit.io/lwjnzkd41/",
-  });
+
 
   return imagekit;
 };
