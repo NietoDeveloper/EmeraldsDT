d08e633604e3949c88ea1903e2e3b7bdae37b2ac
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:25:37 -0500

    ok

--- Backend/models/Category.js
+++ Backend/models/Category.js
@@ -6,22 +6,7 @@ const categorySchema = new mongoose.Schema(
     category: {
       type: String,
       required: true,
-    },
-    image: {
-      fileId: {
-        type: String,
-        required: true,
-      },
-      url: {
-        type: String,
-        required: true,
-      },
-    },
-    position: {
-      type: Number,
-      default: 0,
-    },
-  },
+
   { timestamps: true }
 );
 
