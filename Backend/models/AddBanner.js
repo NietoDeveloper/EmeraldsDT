ca6487ac80d63dc0ed22a280957b36ac0cabc3d2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:25:01 -0500

    Update AddBanner.js

--- Backend/models/AddBanner.js
+++ Backend/models/AddBanner.js
@@ -1,15 +1,1 @@
-import mongoose from "mongoose";
 
-const bannerSchema = new mongoose.Schema({
-  banners: {
-    silderName: {
-      type: String,
-      enum: ["Slide 1", "Slide 2"],
-    },
-    fil
-  },
-});
-
-const Banner = mongoose.model("Banner", bannerSchema);
-
-export default Banner;
