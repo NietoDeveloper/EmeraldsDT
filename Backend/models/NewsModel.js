7f0ae4ef151a1fc31bbb5a8ec28551f120ca5bf7
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:41:37 -0500

    Update NewsModel.js

--- Backend/models/NewsModel.js
+++ Backend/models/NewsModel.js
@@ -1,17 +1,4 @@
 import mongoose from "mongoose";
 
-const newsSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-
-  },
-  {
-    timestamps: true,
-  }
-);
-
 const News = mongoose.model("News", newsSchema);
 export default News;
