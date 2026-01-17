fcf69e5102d5aacb04d9b03bdca71bcb41143500
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:48:23 -0500

    Update productRouter.js

--- Backend/routes/productRouter.js
+++ Backend/routes/productRouter.js
@@ -1,12 +1,4 @@
 import express from "express";
-import {
-  createProduct,
-  getProduct,
-  updateProduct,
-  deleteProduct,
-  getProductById
-} from "../controllers/productController.js";
-
 
 
 export default router;
