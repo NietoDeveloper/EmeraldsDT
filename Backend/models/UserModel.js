5fb692e5d05e5ba4cd871923c427f3bc98120c7e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-16 23:42:58 -0500

    Update UserModel.js

--- Backend/models/UserModel.js
+++ Backend/models/UserModel.js
@@ -1,36 +1,5 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-
-    orders: [
-      {
-        name: {
-          type: String,
-          required: true,
-        },
-        status: {
-          type: String,
-{
-          type: Number,
-          // required: true,
-        },
-   
-        s
-          },
-        ],
-      },
-    ],
-  },
-  {
-    timestamps: true,
-  }
-);
 
 const User = mongoose.model("User", UserSchema);
 export default User;
