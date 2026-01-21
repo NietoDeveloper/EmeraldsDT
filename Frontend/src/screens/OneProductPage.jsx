c8de6d8a360714c38db9d9abb8195178f1134f8a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 19:59:27 -0500

    Update OneProductPage.jsx

--- Frontend/src/screens/OneProductPage.jsx
+++ Frontend/src/screens/OneProductPage.jsx
@@ -2,27 +2,5 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import axios f
-                        <div className="h-8 w-8 absolute top-[30%] left-3 -translate-x-1/3 flex items-center cursor-pointer justify-center">
-                          <FaMinus className="text-[#bebebe] hover:text-red-500" />
-                        </div>
-                      </div>
-                    </div>
-                  </SwiperSlide>
-                ))}
-          </Swiper>
-
-          {/*related product section end */}
-
-          <Footer />
-        </div>
-      )}
-      <AddToCart
-        showModal={showModal}
-        closeModal={closeModel}
-        selectedItem={selectedItem}
-      />
-    </div>
-  );
-};
 
 export default OneProductPage;
