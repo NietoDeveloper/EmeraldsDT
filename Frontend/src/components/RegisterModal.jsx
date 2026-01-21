8482992a54fee64f98292d481483512de144ffea
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 20:31:02 -0500

    Update RegisterModal.jsx

--- Frontend/src/components/RegisterModal.jsx
+++ Frontend/src/components/RegisterModal.jsx
@@ -1,6 +1,3 @@
 import React, { useState } from "react";
 import { IoClose } from "react-icons/io5";
-import axios from "../utils/axios";
-import toast from "react-hot-toast";
-import LoginModal from "./LoginModal";
 
