f6ac942af361be9d262e2c1bc6536d312eadbd54
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 20:28:03 -0500

    Update Hompage.jsx

--- Frontend/src/screens/Hompage.jsx
+++ Frontend/src/screens/Hompage.jsx
@@ -2,9 +2,3 @@ import { useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Menu from "../components/homepage/Menu";
 
-import Footer from "../components/Footer";
-import Section2 from "../components/homepage/Section2";
-
-
-
-export default Hompage;
