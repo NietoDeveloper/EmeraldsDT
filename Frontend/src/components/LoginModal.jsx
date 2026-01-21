689d5dcb0cc6f53e91569472b38dfe8c0e61b555
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 20:32:17 -0500

    Update LoginModal.jsx

--- Frontend/src/components/LoginModal.jsx
+++ Frontend/src/components/LoginModal.jsx
@@ -1,17 +1,1 @@
-import React, { useState } from "react";
-import { IoClose } from "react-icons/io5";
-import axios from "../utils/axios";
-import toast from "react-hot-toast";
 
-const LoginModal = ({ showModal, setShowModal, setIsUserAuthenticated }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const SubmitHandler = async (e) => {
-    e.preventDefault();
-
-    </>
-  );
-};
-
-export default LoginModal;
