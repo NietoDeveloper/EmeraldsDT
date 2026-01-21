6203ebb946a2d55f5429ee5622ca581174e0ffcf
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 19:59:31 -0500

    Update OneProductPage.jsx

--- Frontend/src/screens/OneProductPage.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import axios f
-
-export default OneProductPage;
