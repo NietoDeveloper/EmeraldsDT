e0e32e070e91d5e87f5757f88b253cd4462f1467
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 20:30:30 -0500

    Update RegisterModal.jsx

--- Frontend/src/components/RegisterModal.jsx
+++ Frontend/src/components/RegisterModal.jsx
@@ -41,25 +41,7 @@ const RegisterModal = ({ setShowModal, showModal, setIsUserAuthenticated }) => {
 
   return (
     <>
-      {/* Checkout Modal */}
-      <div
-        className={`fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-gray-400 bg-opacity-50 transition-all duration-300 ease-linear  ${
-          showModal ? "visible" : "invisible"
-        }`}
-        id="modal"
-      >
-        <div
-          className={`w-full px-6 py-4 mx-auto bg-white rounded-3xl shadow-md shadow-color-red md:max-w-md pointer-events-auto transition-all duration-300 delay-200 ${
-            showModal ? "scale-100" : "scale-0"
-          } `}
-        >
-          <div className="mb-4 text-lg font-bold text-center text-gray-700">
-            Register
-          </div>
-          <div className="px-4 py-3 mb-8">
-            <form onSubmit={SubmitHandler}>
-              <label className="block text-sm after:content-[''] after:absolute relative after:h-[1px] after:w-full after:transition-all after:duration-300 after:bg-color-red after:bottom-0 after:left-0  ">
-                <span className="text-gray-700">Full name</span>
+     
                 <input
                   type="text"
                   className="block w-full mt-2 text-sm py-2 outline-none  form-input "
