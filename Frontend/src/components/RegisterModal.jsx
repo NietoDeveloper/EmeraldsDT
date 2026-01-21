70db767a1f8edcc2c081873cc5956f3a3aaba136
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 20:30:55 -0500

    Update RegisterModal.jsx

--- Frontend/src/components/RegisterModal.jsx
+++ Frontend/src/components/RegisterModal.jsx
@@ -4,15 +4,3 @@ import axios from "../utils/axios";
 import toast from "react-hot-toast";
 import LoginModal from "./LoginModal";
 
-const RegisterModal = ({ setShowModal, showModal, setIsUserAuthenticated }) => {
-  const [showLoginModal, setShowLoginModal] = useState(false);
-
-  const showLoginModalHandler = () => {
-    setShowLoginModal(true);
-    setShowModal(false);
-  };
-
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
