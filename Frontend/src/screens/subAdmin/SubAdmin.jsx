a89725dc7bcdde39071c876f86ba937013c00ff4
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 20:27:12 -0500

    Update SubAdmin.jsx

--- Frontend/src/screens/subAdmin/SubAdmin.jsx
+++ Frontend/src/screens/subAdmin/SubAdmin.jsx
@@ -1,30 +1,4 @@
-
-                  <li className="py-2 ps-10 hover:bg-color-red">
-                    <Link to="/subAdmin/allnews" className="px-4 py-2">
-                      All News
-                    </Link>
-                  </li>
-                </ul>
-              )}
-            </li>
-          )}
-
-          {/* Ingredient */}
-          {subAdmin && subAdmin?.ingrdientPermission === true && (
-            <li
-              className={`mb-2 transition-all duration-300 h-10 overflow-hidden ${
-                isIngrdientSubchildVisible ? "h-32" : ""
-              }`}
-              onClick={toggleIngrdientSubchildVisibility}
-            >
-              <div className="flex items-center gap-4 px-4 py-2 cursor-pointer transition-all duration-300 hover:bg-amber-300	hover:text-black">
-                <MdOutlineNoteAlt className="text-xl mr-2" />
-                <span>Ingredient</span>
-                <FaChevronDown className="d-block ms-auto" />
-              </div>
-
-              {isIngrdientSubchildVisible && (
-                <ul className="transition-all duration-300 ">
+"transition-all duration-300 ">
                   <li className="py-2 ps-10 hover:bg-color-red">
                     <Link to="/subAdmin/addingredient" className="px-4 py-2">
                       Add Ingredient
