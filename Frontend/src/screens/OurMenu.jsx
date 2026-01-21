7a500b11b15ad9550445f15be8c3d5416f8a692f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-20 19:57:15 -0500

    Update OurMenu.jsx

--- Frontend/src/screens/OurMenu.jsx
+++ Frontend/src/screens/OurMenu.jsx
@@ -1,11 +1,4 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Hero from "../components/ourMenu/Hero";
-import Footer from "../components/Footer";
-
-const OurMenu = () => {
-  const location = useLocation();
-
-  return (
 
