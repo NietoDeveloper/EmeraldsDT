82b1aa0b7b7b69a4a582b3325b21cafee1058dc5
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:04:32 -0500

    Update DisableOrderModal.jsx

--- Frontend/src/components/DisableOrderModal.jsx
+++ Frontend/src/components/DisableOrderModal.jsx
@@ -7,14 +7,7 @@ const DisableOrderModal = ({ setShowModal, showModal }) => {
       <div
         className={`fixed inset-0 z-50 flex items-center justify-center w-full h-full bg-gray-400 bg-opacity-50 transition-all duration-300 ease-linear  ${
           showModal ? "visible" : "invisible"
-        }`}
-        id="modal"
-      >
 
-
-        {/* Close Model Icon */}
-   
-        </div>
       </div>
     </>
   );
