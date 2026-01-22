7a746b9d3cb52c3e6ec611908318166312ef9016
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:15:33 -0500

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -145,11 +145,7 @@ const Footer = () => {
         <div className="order-2 md:order-3 mt-4 md:mt-0">
           <p className="text-center">
             Managed by
-            <a
-              href="https://manuelnieto.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="text-[#00A36C] hover:text-emerald-700 cursor-pointer font-bold ml-1 transition duration-200"
+ssName="text-[#00A36C] hover:text-emerald-700 cursor-pointer font-bold ml-1 transition duration-200"
             >
               NietoDeveloper
             </a>
