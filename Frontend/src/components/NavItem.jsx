a4d036647f188136811ea284db5a6505516dd36a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:03:25 -0500

    Update NavItem.jsx

--- Frontend/src/components/NavItem.jsx
+++ Frontend/src/components/NavItem.jsx
@@ -13,11 +13,7 @@ function NavItem({ text, menu, link, pdf, setMenu, drop, index }) {
     <li
       className={`capitalize px-5 text-[18px]  h-full font-hobo-light relative transition-all duration-100 cursor-pointer after:lg:block after:hidden flex items-center lg:mt-0 mt-4 ${
         isActive
-     
-        (menu ? (
-          <FaAngleDown className="text-[18px] ml-2 transform rotate-180" />
-        ) : (
-          <FaAngleDown className="text-[18px] ml-2 " />
+     />
         ))}
     </li>
   );
