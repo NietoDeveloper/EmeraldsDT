0b7056860cecf50015ea8ddc7005cf77b7486a79
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:04:43 -0500

    Update DisableOrderModal.jsx

--- Frontend/src/components/DisableOrderModal.jsx
+++ Frontend/src/components/DisableOrderModal.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import { IoClose } from "react-icons/io5";
 
-const DisableOrderModal = ({ setShowModal, showModal }) => {
-  return (
-
-  );
-};
 
 export default DisableOrderModal;
