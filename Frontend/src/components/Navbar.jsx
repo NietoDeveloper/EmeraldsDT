1b9480c2461608a92d43f80d8193250604f6de8e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:14:25 -0500

    Update Navbar.jsx

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,7 +1,3 @@
 import React, { useContext, useEffect, useState } from "react";
 import logo from "../assets/logo.png";
 import { FaRegUser } from "react-icons/fa";
-import { IoBagOutline } from "react-icons/io5";
-import { IoIosMenu } from "react-icons/io";
-import { MdOutlineClose } from "react-icons/md";
-import { Link, useNavigate } from "react-router-dom";
