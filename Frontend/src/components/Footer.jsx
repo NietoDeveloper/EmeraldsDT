90bf1b4a59c861a31a2ac31d819d1dc543266a11
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:18:22 -0500

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,6 +1,3 @@
 import { useEffect, useState } from "react";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
 
-
-export default Footer;
