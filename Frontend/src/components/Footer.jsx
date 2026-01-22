39d04e437967babc646e7e6df9ba11173e2bed74
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:18:14 -0500

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -2,10 +2,5 @@ import { useEffect, useState } from "react";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 
-const Footer = () => {
-  const navigate = useNavigate();
-  const [currentTime, setCurrentTime] = useState("");
-
-
 
 export default Footer;
