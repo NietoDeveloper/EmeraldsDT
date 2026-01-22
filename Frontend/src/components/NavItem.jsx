154052b91c53a9008d440290670a99af11eb44c9
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:03:42 -0500

    Update NavItem.jsx

--- Frontend/src/components/NavItem.jsx
+++ Frontend/src/components/NavItem.jsx
@@ -1,9 +1,3 @@
 import React from "react";
 import { useLocation, Link } from "react-router-dom";
 import { FaAngleDown } from "react-icons/fa6";
-
-function NavItem({ text, menu, link, pdf, setMenu, drop, index }) {
-  const location = useLocation();
-
-  const isActive =
-  
