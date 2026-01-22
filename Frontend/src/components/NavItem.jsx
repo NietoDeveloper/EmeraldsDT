b5d258ed2d13915d9bcf1a2aae79c6e8d1402ebf
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-21 23:03:13 -0500

    Update NavItem.jsx

--- Frontend/src/components/NavItem.jsx
+++ Frontend/src/components/NavItem.jsx
@@ -14,13 +14,7 @@ function NavItem({ text, menu, link, pdf, setMenu, drop, index }) {
       className={`capitalize px-5 text-[18px]  h-full font-hobo-light relative transition-all duration-100 cursor-pointer after:lg:block after:hidden flex items-center lg:mt-0 mt-4 ${
         isActive
           ? "font-hobo-light font-[700] border-b-4 border-b-red-500 text-[18px]"
-          : ""
-      }`}
-      onClick={() => {
-        if (index === 0) {
-          setMenu(!menu);
-        }
-      }}
+  
     >
       <a href={link} download={pdf ? "ABOUT OUR EMERALDS" : undefined}>
         {text}
