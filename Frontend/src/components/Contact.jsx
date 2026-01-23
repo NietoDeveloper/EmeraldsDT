8edbde3773a87915a104ab0a1f698f3bec89fc97
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:49:37 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -2,9 +2,5 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const Contact = () => {
-  useEffect(() => {
-
-};
 
 export default Contact;
