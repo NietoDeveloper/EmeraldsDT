2e3fc941093d4737a34538dbe7dd3eda05450cf1
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:45:41 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -18,12 +18,6 @@ se), 2000);
                 <div className="relative z-0 w-full mb-5 group">
                   <label
 
-                  <select
-                    name="topic"
-                    id="topic"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
-                    required
-                  >
                     <option value="">-- Please select --</option>
                     <option value="special_offers">Special Offers</option>
                     <option value="vouchers">Vouchers</option>
