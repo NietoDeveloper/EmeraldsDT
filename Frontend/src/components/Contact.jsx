e3426f9de47834a16947e7ea0e2e109a1754b197
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:40:35 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -19,8 +19,6 @@ se), 2000);
                     required
                   />
                 </div>
-                <div className="relative z-0 w-full mb-5 group">
-                  <label
                     htmlFor="email"
                     className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
