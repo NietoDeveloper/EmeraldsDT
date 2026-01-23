1ef08b8efe785121ed9b26ec53ae625581c396c9
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:44:10 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -18,10 +18,7 @@ se), 2000);
                 <div className="relative z-0 w-full mb-5 group">
                   <label
                     htmlFor="country_code"
-                    className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
-                    Country Code
-                  </label>
+                  
                   <input
                     type="text"
                     name="country_code"
