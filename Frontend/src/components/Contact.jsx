ae7d43cb7013c9fc1c0955103b9fba1b8c10a8dd
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:44:15 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -20,8 +20,7 @@ se), 2000);
                     htmlFor="country_code"
                   
                   <input
-                    type="text"
-                    name="country_code"
+                    type="t_code"
                     id="country_code"
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
                     placeholder="e.g. 9XX"
