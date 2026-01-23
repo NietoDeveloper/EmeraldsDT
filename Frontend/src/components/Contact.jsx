c8fe2d6a1df4df9a4f0f75fdf043889ecb966687
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:49:06 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -14,9 +14,7 @@ se), 2000);
 
             </div
             <div className="bg-white p-6 rounded-lg shadow-md">
-              <h2 className="text-2xl font-semibold text-gray-800 text-center mb-4">Our Location</h2>
 
-          </div>
         </div>
       </main>
       <Footer />
