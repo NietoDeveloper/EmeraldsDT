0efeadb7d60c0b413da4140fe8a5e00aa2e38053
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:45:04 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -24,9 +24,7 @@ se), 2000);
               
                     type="tel"
                     name="mobile_number"
-                    id="mobile_number"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
-                    placeholder="e.g. 111111111"
+                    id="mobile_num
                   />
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
