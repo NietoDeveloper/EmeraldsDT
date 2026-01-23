297456cf3983520f673f73b335f6b73f8f7fa415
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:45:09 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -22,9 +22,6 @@ se), 2000);
               
                 <div ="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               
-                    type="tel"
-                    name="mobile_number"
-                    id="mobile_num
                   />
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
