1f2459a66e6ccc84447e718a81f2b4f3c7828025
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:44:20 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -21,9 +21,7 @@ se), 2000);
                   
                   <input
                     type="t_code"
-                    id="country_code"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
-                    placeholder="e.g. 9XX"
+                    id="country_co
                   />
                 </div>
                 <div className="relative z-0 w-full mb-5 group">
