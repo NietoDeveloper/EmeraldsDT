f444c1a2c19c882302010e3fbc322e23f52b33aa
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:40:10 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -19,10 +19,7 @@ se), 2000);
                 red
                   />
                 </div>
-                <div className="relative z-0 w-full mb-5 group">
-                  <label
-                    htmlFor="last_name"
-                    className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                <dame="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
                   >
                     * Last Name
                   </label>
