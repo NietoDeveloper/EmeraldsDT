6aa5729b3ca11b31dc0311819a65a13c26d96c83
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:48:13 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -12,16 +12,7 @@ se), 2000);
                    
                 </div>
 
-            </div>
-
-
-
-                </div>
-        ¿
-               
-
-
-            {/* Sección de Mapa */}
+            </div
             <div className="bg-white p-6 rounded-lg shadow-md">
               <h2 className="text-2xl font-semibold text-gray-800 text-center mb-4">Our Location</h2>
               <p className="text-gray-700 text-center mb-4">Santa Marta, Colombia</p>
