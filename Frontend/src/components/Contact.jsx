c8712fe90841de2e3bc322b499753356b63a1ef0
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:40:16 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -18,9 +18,6 @@ se), 2000);
                 target="_blank"
                 red
                   />
-                </div>
-                <dame="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
                     * Last Name
                   </label>
                   <input
