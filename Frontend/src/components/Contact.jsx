a6aad1eb7a912ed669ab22f1e5127a267ab470c0
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-22 23:44:57 -0500

    Update Contact.jsx

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -21,10 +21,7 @@ se), 2000);
                     type="t_code"
               
                 <div ="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-red-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
-                    Mobile Number
-                  </label>
-                  <input
+              
                     type="tel"
                     name="mobile_number"
                     id="mobile_number"
