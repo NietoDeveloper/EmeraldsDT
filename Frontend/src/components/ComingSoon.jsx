f4d875592314066ef054e90738bec02643900941
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-23 23:54:19 -0500

    Update ComingSoon.jsx

--- Frontend/src/components/ComingSoon.jsx
+++ Frontend/src/components/ComingSoon.jsx
@@ -5,10 +5,8 @@ const ComingSoon = () => {
   return (
     <div
       style={{ maxWidth: "1600px" }}
-      className="mx-auto flex justifys-center h-screen  lg:px-44"
+      className="mx-auto flex j  lg:px-44"
     >
-      <div className="flex justiflex-col-reverse items-center w-full">
-        <div className="w-1/2 flex jo ">Coming Soon</h1>
         </div>tify-center items-center ">
           <img src={lgo}className="lg:w-80 w-full " />
         </div>
