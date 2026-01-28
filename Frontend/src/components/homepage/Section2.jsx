f5276d148fc3865c56ac77d5d797b1e98015fdda
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-27 20:32:43 -0500

    Update Section2.jsx

--- Frontend/src/components/homepage/Section2.jsx
+++ Frontend/src/components/homepage/Section2.jsx
@@ -3,6 +3,3 @@ import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
