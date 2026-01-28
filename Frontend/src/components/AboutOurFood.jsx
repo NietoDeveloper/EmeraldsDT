ab10070743e8bd65dbe85177db486d3336c3292c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-27 20:25:49 -0500

    Update AboutOurFood.jsx

--- Frontend/src/components/AboutOurFood.jsx
+++ Frontend/src/components/AboutOurFood.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import AllergensGuidePDF from "../assets/AllergensGuide.pdf";
-
-const AboutOurFood = () => {
+tOurFood = () => {
   retu
