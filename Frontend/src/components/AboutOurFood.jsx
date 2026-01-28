241d5a71e75dfdec389772e8d358019652e5999b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-27 20:25:23 -0500

    Update AboutOurFood.jsx

--- Frontend/src/components/AboutOurFood.jsx
+++ Frontend/src/components/AboutOurFood.jsx
@@ -12,11 +12,7 @@ const AboutOurFood = () => {
       <div className="overflow-hidden lg:px-40">
         <Navbar />
         {/* here i want to on send the link whare user can click and download pdf */}
-        <div className="flex justify-center items-center mt-10">
-          <a href={AllergensGuidePDF} target="_blank" rel="noopener noreferrer">
-            <button className="bg-color-red py-2 px-5 rounded-md text-white font-hobo mt-2 -ml-1 uppercase">
-              Download PDF
-            </button>
+        <div
           </a>
         </div>
         <Footer />
