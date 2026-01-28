6ba2ad13f6d96978e92be4c6348c9ad5b2ec5c79
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-27 20:32:39 -0500

    Update Section2.jsx

--- Frontend/src/components/homepage/Section2.jsx
+++ Frontend/src/components/homepage/Section2.jsx
@@ -6,7 +6,3 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-// import required modules
-import { Pagination, Navigation, Autoplay } from "swiper/modules";
-import { Link } from "react-router-dom";
-import axios from "../../utils/axios";
