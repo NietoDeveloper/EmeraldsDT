a4602c2e74226294e3bc0f4101e86d99dc051118
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-28 23:23:55 -0500

    Update App.jsx

--- Frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Hompage from "./screens/Hompage";
-import OurMenu from "./screens/OurMenu";
-import Admin from "./screens/admin/Admin";
-import DashboardView from "./screens/admin/DashboardView";
-import AddItems from "./screens/admin/items/AddItems";
-import Signin from "./screens/admin/Signin";
-import Merchandise from "./screens/Merchandise";
-import AllItems from "./screens/admin/items/AllItems";
