ea0a6f0ba6ee564a743836f0449ea4c12efd5453
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-29 20:15:11 -0500

    Update Main.jsx

--- Frontend/src/components/ourMenu/Main.jsx
+++ Frontend/src/components/ourMenu/Main.jsx
@@ -1,13 +1,3 @@
-import React, { useEffect, useState } from "react";
-import { FaMinus, FaPlus } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
-import Footer from "../Footer";
-import AddToCart from "./AddToCart";
-import axios from "../../utils/axios";
-
-const Main = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
 
 
 
