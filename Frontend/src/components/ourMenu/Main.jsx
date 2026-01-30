43f52522cb0483a9947b6a53f98e61688b4681be
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-29 20:15:05 -0500

    Update Main.jsx

--- Frontend/src/components/ourMenu/Main.jsx
+++ Frontend/src/components/ourMenu/Main.jsx
@@ -8,23 +8,6 @@ import axios from "../../utils/axios";
 const Main = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
-
-  const [selectedItem, setSelectedItem] = useState({});
-  const [loading, setLoading] = useState(true);
-
-
-
-  const navigate = useNavigate();
-
-  const closeModel = (e) => {
-    setShowModal(false);
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get("/admin/getItem");
 
 
 
