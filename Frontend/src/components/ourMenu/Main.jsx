b014be312a1c8691cc711247e73b0f276397b9fd
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-29 20:14:59 -0500

    Update Main.jsx

--- Frontend/src/components/ourMenu/Main.jsx
+++ Frontend/src/components/ourMenu/Main.jsx
@@ -25,29 +25,6 @@ const Main = () => {
     const fetchData = async () => {
       try {
         const { data } = await axios.get("/admin/getItem");
-        const uniqueCategories = [
-          ...new Set(data.items.map((item) => item.category)),
-        ];
-        const categoryNames = await Promise.all(
-          uniqueCategories.map(async (category) => {
-            const { data } = await axios.get(
-              `/admin/getCategoryById/${category}`
-            );
-            return data.category;
-          })
-        );
-        // Sort categoryNames based on position
-        const sortedCategoryNames = categoryNames.sort(
-          (a, b) => a.position - b.position
-        );
-        setState({
-          data,
-          categories: uniqueCategories,
-          categoryNames: sortedCategoryNames,
-        });
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
 
 
 
