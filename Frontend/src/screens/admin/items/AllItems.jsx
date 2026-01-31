1a0bda458b3dd475db0edf9d038293fc259920b2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-30 23:34:24 -0500

    Update AllItems.jsx

--- Frontend/src/screens/admin/items/AllItems.jsx
+++ Frontend/src/screens/admin/items/AllItems.jsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { MdDelete } from "react-icons/md";
-import axios from "../../../utils/axios";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
-import UpdateModal from "./UpdateModal";
-
-const MySwal = withReactContent(Swal);
 
 
 
