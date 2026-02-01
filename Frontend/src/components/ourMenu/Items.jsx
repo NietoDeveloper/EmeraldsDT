255bba65c680666832a86c55c7c43c5a17e047f7
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-01-31 21:52:04 -0500

    Update Items.jsx

--- Frontend/src/components/ourMenu/Items.jsx
+++ Frontend/src/components/ourMenu/Items.jsx
@@ -1,11 +1,3 @@
 import React, { useEffect, useState } from "react";
 import axios from "../../utils/axios";
 import { useNavigate } from "react-router-dom";
-import { FaMinus, FaPlus } from "react-icons/fa";
-import AddToCart from "./AddToCart";
-
-const Items = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
