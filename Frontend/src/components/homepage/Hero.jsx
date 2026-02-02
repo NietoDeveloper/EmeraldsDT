5c96487acedd0120512feacedeca365fe2a6b386
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-02 17:25:13 -0500

    Update Hero.jsx

--- Frontend/src/components/homepage/Hero.jsx
+++ Frontend/src/components/homepage/Hero.jsx
@@ -5,14 +5,6 @@ import axios from "../../utils/axios";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import { Pagination, Navigation, Autoplay } from "swiper/modules";
-
-const Hero = () => {
-  const [banner, setBanner] = useState([]);
 
 
 
