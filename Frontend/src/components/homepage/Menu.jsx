d8ef3238ef7e99e18ecb14d18b85a58789314fdc
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-02 17:29:48 -0500

    Update Menu.jsx

--- Frontend/src/components/homepage/Menu.jsx
+++ Frontend/src/components/homepage/Menu.jsx
@@ -7,11 +7,7 @@ import NewNews from "./NewNews";
 const Menu = () => {
   return (
     <div className="h-[auto] w-full flex flex-col items-end relative lg:mt-24 mt-10">
-      <div className=" w-full lg:px-10 px-5 lg:pr-5 relative">
-        <MenuItems />
-      </div>
 
-    </div>
   );
 };
 
