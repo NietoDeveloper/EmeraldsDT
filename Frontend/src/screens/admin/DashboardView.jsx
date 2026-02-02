ef8b33e56c1d854bb6b215332382a58b461df75b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-01 19:15:11 -0500

    Update DashboardView.jsx

--- Frontend/src/screens/admin/DashboardView.jsx
+++ Frontend/src/screens/admin/DashboardView.jsx
@@ -8,33 +8,5 @@ import { FaLock, FaUnlock } from "react-icons/fa";
 const DashboardView = () => {
   const UserData = [
     {
-      id: 1,
-      name: "Total Users",
-      userGain: 100,
-    },
-
-  ];
-
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.name),
-    datasets: [
-      {
-        borderWidth: 1,
-      },
-    ],
-  });
-
-
-        display: true,
-        text: "All Details",
-      },
-    },
-  };
-
-  const [adminData, setAdminData] = useState({});
-
-    fetchAdminData();
-  }, []);
-
 
 
