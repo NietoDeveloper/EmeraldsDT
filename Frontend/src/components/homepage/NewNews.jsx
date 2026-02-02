bf3d912490130b8124f0fe3abde0649d9b72c75e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-01 19:07:34 -0500

    Update NewNews.jsx

--- Frontend/src/components/homepage/NewNews.jsx
+++ Frontend/src/components/homepage/NewNews.jsx
@@ -1,7 +1,1 @@
 import React, { useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Pagination, Navigation, Autoplay } from "swiper/modules";
-import { Link } from "react-router-dom";
