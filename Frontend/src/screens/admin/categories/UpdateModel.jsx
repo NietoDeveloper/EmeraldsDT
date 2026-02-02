08efeabc65fec234a962820e1aa2df018f44d76b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-01 19:19:27 -0500

    Update UpdateModel.jsx

--- Frontend/src/screens/admin/categories/UpdateModel.jsx
+++ Frontend/src/screens/admin/categories/UpdateModel.jsx
@@ -1,11 +1,3 @@
 import React, { useEffect, useState } from "react";
 import axios from "../../../utils/axios";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
-
-const MySwal = withReactContent(Swal);
-
-const UpdateModel = ({ toggleModal, isOpen, selectedCategory }) => {
-  const [categoryImage, setCategoryImage] = useState(null);
-  const [categoryName, setCategoryName] = useState("");
 
