a937e3ff3e264658368a2df07ab5ee794336afb2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-01 19:16:13 -0500

    Update UpdateModel.jsx

--- Frontend/src/screens/admin/categories/UpdateModel.jsx
+++ Frontend/src/screens/admin/categories/UpdateModel.jsx
@@ -87,15 +87,7 @@ const UpdateModel = ({ toggleModal, isOpen, selectedCategory }) => {
                 >
                   Category Position
                 </label>
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline ring-1 ring-black"
-                  id="itemName"
-                  type="text"
-                  placeholder={selectedCategory.position}
-                  onChange={(e) => setCategoryPosition(e.target.value)}
-                  value={categoryPosition}
-                />
-              </div>
+
 
               <div className="mb-4">
                 <label className="block text-gray-900 text-sm font-bold mb-2">
