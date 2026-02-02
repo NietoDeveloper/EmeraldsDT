1865ca46460f0f1f7b338dc4ea159c1b0dc7edfe
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-02 17:18:45 -0500

    Update AddToCart.jsx

--- Frontend/src/components/ourMenu/AddToCart.jsx
+++ Frontend/src/components/ourMenu/AddToCart.jsx
@@ -1,9 +1,1 @@
-import React, { useState, useEffect, useContext } from "react";
-import { FaMinus, FaPlus } from "react-icons/fa";
-import { IoMdClose } from "react-icons/io";
-import { IoCheckmarkCircle } from "react-icons/io5";
-import { DataContext } from "../../ContexApi";
-import { useLocation } from "react-router-dom";
-import toast from "react-hot-toast";
-
 
