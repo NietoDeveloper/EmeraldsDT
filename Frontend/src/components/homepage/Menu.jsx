48ecff4c72acf2b57c8fcc33c24d373fc6c58b74
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-02 17:30:13 -0500

    Update Menu.jsx

--- Frontend/src/components/homepage/Menu.jsx
+++ Frontend/src/components/homepage/Menu.jsx
@@ -1,4 +1,2 @@
 import React from "react";
-import img1 from "../../assets/Menu1.png";
-import { Link } from "react-router-dom";
-import MenuItems from "./MenuItems";
+port MenuItems from "./MenuItems";
