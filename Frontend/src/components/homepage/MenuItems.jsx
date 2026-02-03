e79a342793d4c423b640a8303c9717660a062162
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-03 06:49:35 -0500

    Update MenuItems.jsx

--- Frontend/src/components/homepage/MenuItems.jsx
+++ Frontend/src/components/homepage/MenuItems.jsx
@@ -1,8 +1,1 @@
-import React, { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
 
-
-
-export default MenuItems;
