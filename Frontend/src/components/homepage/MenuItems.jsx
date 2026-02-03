e48f9c699e6adb2fc4c61e21f2f3c0f47e6edacb
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-03 06:49:29 -0500

    Update MenuItems.jsx

--- Frontend/src/components/homepage/MenuItems.jsx
+++ Frontend/src/components/homepage/MenuItems.jsx
@@ -2,10 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import axios from "../../utils/axios";
-import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
 
 
