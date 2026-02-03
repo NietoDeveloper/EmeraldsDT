53a56323b476c41e967d08db1d3f138587a311ea
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-03 06:49:09 -0500

    Update MenuItems.jsx

--- Frontend/src/components/homepage/MenuItems.jsx
+++ Frontend/src/components/homepage/MenuItems.jsx
@@ -19,32 +19,6 @@ const MenuItems = () => {
         const { data } = await axios.get("/admin/getCategory");
         setCategories(data.categories);
       } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <>
-      <div className="flex justify-between items-center">
-        <h1 className="lg:text-3xl text-2xl font-hobo text-color-red">
-          Our Menu
-        </h1>
-        <Link to="/our-menu/main">
-          <button className="bg-color-red text-white max-sm:text-sm px-3 py-2 rounded-md font-hobo">
-            View All Menu
-          </button>
-        </Link>
-      </div>
-
-      {loading ? (
-        <div className="flex justify-between items-center ">
-
-
 
 
 
