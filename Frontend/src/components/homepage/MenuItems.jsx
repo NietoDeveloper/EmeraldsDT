87f0373d3111db73cb022ce0095c65955643e716
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-03 06:38:01 -0500

    Update MenuItems.jsx

--- Frontend/src/components/homepage/MenuItems.jsx
+++ Frontend/src/components/homepage/MenuItems.jsx
@@ -49,17 +49,13 @@ const MenuItems = () => {
           >
             <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded "></div>
             <div className="h-2.5 bg-gray-200 rounded-full  w-48 mb-4" />
-            <div className="h-2 bg-gray-200 rounded-full  mb-2.5" />
-            <div className="h-2 bg-gray-200 rounded-full  mb-2.5" />
 
-            <div className="flex items-center mt-4">
-              <div>
                 <div className="h-2.5 bg-gray-200 rounded-full  w-32 mb-2" />
                 <div className="w-48 h-2 bg-gray-200 rounded-full " />
               </div>
             </div>
             <span className="sr-only">Loading...</span>
-          </div>
+
           <div
             role="status"
             className=" w-[33%]  p-10   rounded animate-pulse md:p-6 "
