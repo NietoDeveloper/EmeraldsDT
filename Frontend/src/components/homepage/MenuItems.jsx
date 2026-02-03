274fb6155e73e7ef5f18358035040695b6fe579f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-03 06:28:13 -0500

    Update MenuItems.jsx

--- Frontend/src/components/homepage/MenuItems.jsx
+++ Frontend/src/components/homepage/MenuItems.jsx
@@ -85,7 +85,7 @@ const MenuItems = () => {
             <div className="h-2.5 bg-gray-200 rounded-full  w-48 mb-4" />
             <div className="h-2 bg-gray-200 rounded-full  mb-2.5" />
             <div className="h-2 bg-gray-200 rounded-full  mb-2.5" />
-            <div className="h-2 bg-gray-200 rounded-full " />
+
             <div className="flex items-center mt-4">
               <div>
                 <div className="h-2.5 bg-gray-200 rounded-full  w-32 mb-2" />
