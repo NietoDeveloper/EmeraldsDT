372a6b6a890c7f237fd8c6fec6993bb6e7104123
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-22 22:36:50 -0500

    Update HeroSection.tsx

--- Front/src/components/sections/HeroSection.tsx
+++ Front/src/components/sections/HeroSection.tsx
@@ -3,13 +3,6 @@ import { useState, useRef } from "react";
 import { MainButton } from '@/components/ui/MainButton';
 import { MuteToggle } from '@/components/ui/MuteToggle';
 
-export const HeroSection = () => {
-  // Iniciamos en true para asegurar que el Autoplay funcione siempre
-  const [isMuted, setIsMuted] = useState(true);
-  const videoRef = useRef<HTMLVideoElement>(null);
 
-
-le isMuted={isMuted} onToggle={toggleMute} />
-    </section>
   );
 };
