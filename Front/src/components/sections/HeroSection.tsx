de15456e101de35ce1277b7721bb5a59f0572a87
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-22 22:03:49 -0500

    Update HeroSection.tsx

--- Front/src/components/sections/HeroSection.tsx
+++ Front/src/components/sections/HeroSection.tsx
@@ -1,10 +1,1 @@
 "use client";
-import { MainButton } from '@/components/ui/MainButton';
-
-export const HeroSection = () => {
-  return (
-    <section className="relative h-screen w-full overflow-hidden flex flex-col justify-end pb-20">
-
-
-  );
-};
