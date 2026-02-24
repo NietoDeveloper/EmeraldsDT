45a256a3706de55b3742f0b78bb7d2489b8c34ef
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-23 23:54:24 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -10,15 +10,3 @@ export const Navbar = () => {
   const scrollDirection = useScrollDirection();
   const [isAtTop, setIsAtTop] = useState(true);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => setIsAtTop(window.scrollY < 20);
-    handleScroll();
-    window.addEventListener("scroll", handleScroll, { passive: true });
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const handleLogoClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    window.location.href = "/";
-  };
