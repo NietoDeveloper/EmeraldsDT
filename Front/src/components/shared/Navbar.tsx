fcc0b05a4c33de40834f94979db55eceeb4f115c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-23 20:24:34 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -17,11 +17,7 @@ import { useState, useEffect } from "react";
         </div>
 
         <div className="flex items-center gap-6">
-         
-          <button className="lg:hidden flex flex-col gap-1.5 items-end group">
-            <div className="w-8 h-[1px] bg-white group-hover:bg-gold transition-colors" />
-            <div className="w-5 h-[1px] bg-white group-hover:bg-emerald transition-colors" />
-          </button>
+
         </div>
 
       </div>
