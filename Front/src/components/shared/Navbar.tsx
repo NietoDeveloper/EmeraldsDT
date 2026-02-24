10f6b13e7e97ca70d8407f510b65d02627590da6
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-23 20:36:22 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -6,9 +6,4 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { ShoppingCart, User, Menu, X, History, Package } from "lucide-react";
 
-export const Navbar = () => {
-  const scrollDirection = useScrollDirection();
-  const [isAtTop, setIsAtTop] = useState(true);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
 };
