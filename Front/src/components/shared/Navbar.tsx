28abb8515dcb1bc2da53a1b90a4e195596bd72c0
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-23 20:24:17 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -13,14 +13,6 @@ import { useState, useEffect } from "react";
   return (
     <nav
 
-      <div className="max-w-[1600px] mx-auto flex justify-between items-center">
-        
-   
-      
-          <span className="font-bold tracking-[0.4em] uppercase text-lg md:text-xl transition-all duration-500 group-hover:tracking-[0.45em]">
-            Emerald <span className="text-white/40 group-hover:text-gold transition-colors italic font-light">DT</span>
-          </span>
-        </Link>
 
         </div>
 
