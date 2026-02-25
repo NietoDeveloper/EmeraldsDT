664cf44b3bb61ed8a3aff5c32983aaefc50d9f7d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-25 15:16:38 -0500

    Update postcss.config.mjs

--- Front/postcss.config.mjs
+++ Front/postcss.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: 
+  plugins: {
+    // Usamos los nombres de los plugins como keys para compatibilidad total con PostCSS 8
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
 
 export default config;
