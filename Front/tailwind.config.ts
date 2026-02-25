35d99ac8bb72a13e2618e3843dfb0dddab1e678c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-25 17:40:25 -0500

    Update tailwind.config.ts

--- Front/tailwind.config.ts
+++ Front/tailwind.config.ts
@@ -1,23 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    // Definimos pantallas explícitas para el Nieto Lab
-    screens: {
-      'xs': '310px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-      'uw': '1900px', // Ultra-Wide: El estándar de tu proyecto
-    },
-
 
       },
     },
