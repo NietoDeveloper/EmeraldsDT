fa7d6efade9ea2dab31c91bb9eeb37f0c07d4812
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-02-24 23:59:46 -0500

    Update layout.tsx

--- Front/src/app/layout.tsx
+++ Front/src/app/layout.tsx
@@ -3,10 +3,4 @@ import { Inter, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/shared/Navbar";
 
-const sans = Inter({
-  variable: "--font-sans",
-  subsets: ["latin"],
-  display: 'swap',
-});
-
 const mono = JetBrains_Mono({
