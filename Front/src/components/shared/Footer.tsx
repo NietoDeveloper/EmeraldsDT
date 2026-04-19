b8d0ae28a7c075be0ad523be517d7138e7b5e51d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-18 23:56:35 -0500

    Update Footer.tsx

--- Front/src/components/shared/Footer.tsx
+++ Front/src/components/shared/Footer.tsx
@@ -25,8 +25,7 @@ export const Footer = () => {
       hour: '2-digit',
       minute: '2-digit',
       second: '2-digit',
-      day: '2-digit',
-          
+         
   
             
           
