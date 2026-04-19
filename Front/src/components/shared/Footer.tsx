54e034ff50ffbdd1537bec7c59f8b4d20f6761df
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-18 23:55:55 -0500

    Update Footer.tsx

--- Front/src/components/shared/Footer.tsx
+++ Front/src/components/shared/Footer.tsx
@@ -36,10 +36,7 @@ export const Footer = () => {
     <footer className="w-full bg-black text-gold border-t border-emerald/30 pt-12 pb-8 md:pt-20 md:pb-12 font-sans relative box-border overflow-hidden">
       
       <div className="w-full px-6 sm:px-12 md:px-24 lg:px-32 xl:px-48 max-w-[1900px] mx-auto transition-all duration-500">
-        
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-12 lg:gap-20 mb-16">
-          
-
+ 
            
   
             
