aaa4063ce5c7f52e5fc740a7cc79af9e870296c4
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-18 23:57:30 -0500

    Update Footer.tsx

--- Front/src/components/shared/Footer.tsx
+++ Front/src/components/shared/Footer.tsx
@@ -2,10 +2,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
-import { Mail, ExternalLink, ShieldCheck, Cpu } from 'lucide-react';
-/**
-*/
+
 
   
             
