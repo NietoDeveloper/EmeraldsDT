2a35573901325fd75ee44f344f34f299d95ead59
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-18 23:57:16 -0500

    Update Footer.tsx

--- Front/src/components/shared/Footer.tsx
+++ Front/src/components/shared/Footer.tsx
@@ -6,12 +6,7 @@ import Link from 'next/link';
 import { Mail, ExternalLink, ShieldCheck, Cpu } from 'lucide-react';
 
 /**
- * Emerald DT - Footer Final 
- * Calibrado para: 310px - 1900px
- * Author: NietoDeveloper
- */
-export const Footer = () => {
-// FIX: Definición de currentYear para evitar ReferenceError
+*/
 
   
             
