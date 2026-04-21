9c407f890120b4453b30d1d67e9596b167730d74
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-20 23:41:49 -0500

    Update layout.tsx

--- emerald-dt-dashboard/app/layout.tsx
+++ emerald-dt-dashboard/app/layout.tsx
@@ -8,10 +8,7 @@ export default function RootLayout({
 }: 
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+      lang="en"ol">{children}</body>
     </html>
   );
 }
