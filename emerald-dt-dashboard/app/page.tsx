2fcab92d80cd851bc29a4fabc412de3a2a4331a2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-20 23:39:35 -0500

    Update page.tsx

--- emerald-dt-dashboard/app/page.tsx
+++ emerald-dt-dashboard/app/page.tsx
@@ -6,14 +6,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get start
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
+          <h1 className="max-w-xs t
               Learning
             </a>{" "}
             center.
