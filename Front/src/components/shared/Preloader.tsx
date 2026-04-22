5a8917d90dd548ba4cc1fdd022eaa19d7285c8bd
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-21 20:59:06 -0500

    Update Preloader.tsx

--- Front/src/components/shared/Preloader.tsx
+++ Front/src/components/shared/Preloader.tsx
@@ -9,13 +9,6 @@ export default function Preloader() {
   const [isMounted, setIsMounted] = useState(false);
 
 .div>
-
-            <motion.div
-              initial={{ y: 20, opacity: 0 }}
-              ani
-ation: 2.5, repeat: Infinity }}
-                className="mt-6 text-[9px] text-emerald-300/30 font-mono tracking-[0.4em] uppercase"
-              >
       )}
     </AnimatePresence>
   );
