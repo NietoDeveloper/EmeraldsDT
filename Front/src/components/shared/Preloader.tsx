c4110b1d991d5990da489e4e9e5c10796e6fa941
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-21 20:59:00 -0500

    Update Preloader.tsx

--- Front/src/components/shared/Preloader.tsx
+++ Front/src/components/shared/Preloader.tsx
@@ -13,20 +13,9 @@ export default function Preloader() {
             <motion.div
               initial={{ y: 20, opacity: 0 }}
               ani
-
-              {/* BARRA DE PROGRESO - CARGA EN 4 SEGUNDOS */}
-    
-              
-              <motion.p 
-                animate={{ opacity: [0.2, 0.8, 0.2] }}
-                transition={{ duration: 2.5, repeat: Infinity }}
+ation: 2.5, repeat: Infinity }}
                 className="mt-6 text-[9px] text-emerald-300/30 font-mono tracking-[0.4em] uppercase"
               >
-                {pathname === '/' ? 'Establishing Secure Link' : `Syncing ${pathname}`}
-              </motion.p>
-            </motion.div>
-          </div>
-        </motion.div>
       )}
     </AnimatePresence>
   );
