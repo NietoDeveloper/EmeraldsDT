767cd7a42d86f6f8d51ffe8474894e0e2efa1f32
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-21 20:59:19 -0500

    Update Preloader.tsx

--- Front/src/components/shared/Preloader.tsx
+++ Front/src/components/shared/Preloader.tsx
@@ -1,10 +1,2 @@
 'use client';
-import { motion, AnimatePresence } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
-import Image from 'next/image';
-
-export default function Preloader() {
-  const [loadince>
-  );
-}
+import { motion, A
