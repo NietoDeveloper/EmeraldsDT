16b247f2a702d0b8b0ecce726e364a9f611cc179
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-22 23:53:42 -0500

    Update layout.tsx

--- emerald-dt-dashboard/src/app/layout.tsx
+++ emerald-dt-dashboard/src/app/layout.tsx
@@ -1,14 +1,5 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./style.css";
-
-export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
-  themeColor: "#000000",
-};
 
 export default function RootLayout({
   children,
