87e500f466006f732741cd630b2fcc351f585a8a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-23 23:43:43 -0500

    Update layout.tsx

--- emerald-dt-dashboard/src/app/layout.tsx
+++ emerald-dt-dashboard/src/app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
+
   userScalable: false,
   themeColor: "#000000",
 };
