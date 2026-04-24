1665005b5403588b422d663599feaaca58b16c23
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-24 02:50:44 -0500

    Update DashboardCard.tsx

--- emerald-dt-dashboard/src/components/DashboardCard.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React, { useState } from 'react';
-
-interface DashboardCardProps {
-  children: React.ReactNode;
-  title: string;
-  defaultColSpan?: string; // e.g., "col-span-2"
-  defaultRowSpan?: string; // e.g., "row-span-1"
