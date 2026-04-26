ec06a797418207de74ec95d57c9d16588474301c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-25 23:52:46 -0500

    Update database.tsx

--- Back/src/config/database.tsx
+++ Back/src/config/database.tsx
@@ -3,5 +3,5 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
   tr
     const conn = await mongoose.conrocess.env.MONGO_URI || '');
-    console.log(`📡 Clusthnectado: ${conn.cnh);
+    console.log(`📡 Clusthnectado: ${conn
   } catch
