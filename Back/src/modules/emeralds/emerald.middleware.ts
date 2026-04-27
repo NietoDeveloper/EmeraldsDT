9320706118a4d9ba7bf75e322f76916015a1af13
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-26 23:23:56 -0500

    Update emerald.middleware.ts

--- Back/src/modules/emeralds/emerald.middleware.ts
+++ Back/src/modules/emeralds/emerald.middleware.ts
@@ -2,11 +2,3 @@ import { Request, Response, NextFunction } from 'express';
 
 /**
  * 🛰️ LA CONSTRICTOR - PROTOCOLO DE VALIDACIÓN S+
- * Filtra y asegura la integridad de los activos en el inventario.
- */
-export const validateEmeraldData = (req: Request, res: Response, next: NextFunction) => {
-    const { sku, name, specifications, financials } = req.body;
-
- 
-
-};
