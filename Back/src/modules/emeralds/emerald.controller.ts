bf170e3dabccdbe8f4ff4d4401ca4a99f2e4ab24
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-27 19:03:33 -0500

    Update emerald.controller.ts

--- Back/src/modules/emeralds/emerald.controller.ts
+++ Back/src/modules/emeralds/emerald.controller.ts
@@ -2,7 +2,3 @@ import { Request, Response } from 'express';
 import { Emerald } from './emerald.model.js';
 import { storageService } from '../../shared/services/storage.service.js';
 
-/**
- * 🐍 EMERALD CONTROL CENTER - NIVEL S+
- * Operado bajo el protocolo de La Constrictor: Isabella Nieto
- *
