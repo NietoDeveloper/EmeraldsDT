c3b3e52d673b7f144b99b0c6a2d1dd2d02ac7c19
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-27 19:08:30 -0500

    Update emerald.controller.ts

--- Back/src/modules/emeralds/emerald.controller.ts
+++ Back/src/modules/emeralds/emerald.controller.ts
@@ -1,15 +1,2 @@
 import { Request, Response } from 'express';
 import { Emerald } from './emerald.model.js';
-import { storageService } from '../../shared/services/storage.service.js';
-import mongoose from 'mongoose';
-
-/**
- * 🐍 EMERALD CONTROL CENTER - NIVEL L6 (INDUSTRIAL GRADE)
- * Protocolo Constrictor: Isabella Nieto | Software DT
- */
-
-// 1. REGISTRO DE NUEVOS ACTIVOS (CREATE)
-export const createEmerald = async (req: Request, res: Response) => {
-    // Iniciamos sesión para transaccionalidad (Si el cluster soporta replica sets)
-    const session = await mongoose.startSession();
-    session.startTransaction();
