de2f7dd4d9c51b86383812f45fa60b2354434a1d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-04-28 23:51:53 -0500

    Update emerald.controller.ts

--- Back/src/modules/emeralds/emerald.controller.ts
+++ Back/src/modules/emeralds/emerald.controller.ts
@@ -2,8 +2,3 @@ import { Request, Response } from 'express';
 import { Emerald } from './emerald.model.js';
 import { storageService } from '../../shared/services/storage.service.js';
 import mongoose from 'mongoose';
-
-/**
- * 🐍 EMERALD CONTROL CENTER - NIVEL L6 (INDUSTRIAL GRADE)
- * Protocolo Constrictor: Isabella Nieto | Software DT
- */
