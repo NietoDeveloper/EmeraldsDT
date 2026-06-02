dee016c2b314d10b7bcf7eb7d2d9c9518bff6e6a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-01 19:39:17 -0500

    Update auth.routes.ts

---
+++ Back/src/modules/auth/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { login, register } from './auth.controller.js';
+import { validate } from '../../shared/middlewares/validate.middleware.js';
+import { loginSchema, registerSchema } from '../users/user.schema.js';
+
+const router = Router();
+
+// Rutas de Identidad protegidas en la entrada por Zod Shield
+router.post('/register', validate(registerSchema), register);
+router.post('/login', validate(loginSchema), login);
+
+export default router;
