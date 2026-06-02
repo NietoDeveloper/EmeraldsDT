3ab097dade8ed3862b478b1e82e5957a4a2da2d3
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-01 19:33:42 -0500

    Update emerald.routes .ts

--- Back/src/modules/emeralds/emerald.routes .ts
+++ Back/src/modules/emeralds/emerald.routes .ts
@@ -11,6 +11,4 @@ import { emeraldSchema, updateEmeraldSchema } from './emerald.schema.js';
 
 /**
  * 🛠️ ASYNC WRAPPER - SOFTWARE DT STANDARD
- * Asegura que los errores de los controladores lleguen al Global Error Handler.
- */
 };
