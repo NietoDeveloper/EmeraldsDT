be0e1e60e0db346673e7dbd92bca9de9a21108f8
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-01 19:19:25 -0500

    Create user.schema.ts

---
+++ Back/src/modules/users/user.schema.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+    body: z.object({
+        name: z.string().min(2, 'Name must be at least 2 characters long'),
+        email: z.string().email('Invalid industrial email address'),
+        password: z.string().min(8, 'Password must be minimum 8 characters and include high complexity'),
+        role: z.enum(['SUPER_ADMIN', 'EMPLOYEE', 'CLIENT']).optional()
+    })
+});
+
+export const loginSchema = z.object({
+    body: z.object({
+        email: z.string().email('Invalid format'),
+        password: z.string().min(1, 'Password field is mandatory')
+    })
+});
