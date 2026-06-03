d506263449287330191a01e7fe8b50168f7338b2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-02 23:41:09 -0500

    Update app.ts

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -76,8 +76,7 @@ app.get('/', (_req: Request, res: Response) => {
  * 💥 GLOBAL ERROR HANDLER (L6 Practice)
  * Captura fallos en el pipeline y evita fugas de información
  */
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.statusCode || 500;
+app.use((err: any, req: Request, res
     console.error(`\x1b[31m[Critical Error]: ${err.message}\x1b[0m`);
     
 
