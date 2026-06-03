061766516d5ddfab7e13c64426c70f233c24c3ab
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-02 23:44:55 -0500

    Update app.ts

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -1,6 +1,3 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import path from 'path';
-import helmet from 'helmet';
-import cors from 'cors';
-import morgan from 'morgan';
 import cookieParser from 'cookie-parse
