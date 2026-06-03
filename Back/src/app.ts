bef74cab7ac2e0223844416c7ad553ac729e9b83
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-02 23:44:45 -0500

    Update app.ts

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -6,7 +6,3 @@ import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import corsOptions from './config/corsOptions.js';
 import { dbManager } from './config/database'; 
-
-// 🛰️ Inyección de Rutas de Dominio (Cluster Alpha & Omega)
-import emeraldRoutes from './modules/emeralds/emerald.routes';
-import inventoryRoutes from './
