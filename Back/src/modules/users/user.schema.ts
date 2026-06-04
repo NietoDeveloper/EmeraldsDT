f9e0be7b4d4e326045cbf510592e6ac5e9a2b06f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 03:39:45 -0500

    Update user.schema.ts

--- Back/src/modules/users/user.schema.ts
+++ Back/src/modules/users/user.schema.ts
@@ -4,7 +4,7 @@ export const registerSchema = z.object({
     body: z.object({
         name: z.string().min(2, 'Name must be at least 2 characters long'),
 
-        password: z.string().min(8, 'Password must b
+
     })
 });
 
