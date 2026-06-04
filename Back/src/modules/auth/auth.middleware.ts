341ca8588117eee32df187f5b423e558ce3ee3ab
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 15:02:46 -0500

    Update auth.middleware.ts

--- Back/src/modules/auth/auth.middleware.ts
+++ Back/src/modules/auth/auth.middleware.ts
@@ -6,6 +6,4 @@ import { AuthenticatedRequest, IJWTPayload } from './auth.interfaces.js';
 export const requireAuth = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try {
         const authHeader = req.headers.authorization;
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return res.status(401).json({ status: 'UNAUTHORIZED', message: 'Missing or malformed token' });
-        }
+  
