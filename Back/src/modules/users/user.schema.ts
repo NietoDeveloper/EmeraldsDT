9f2c104221acd87c0944f8dd17e4d3520becffb0
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 03:39:39 -0500

    Update user.schema.ts

--- Back/src/modules/users/user.schema.ts
+++ Back/src/modules/users/user.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const registerSchema = z.object({
     body: z.object({
         name: z.string().min(2, 'Name must be at least 2 characters long'),
-        email: z.string().email('Invalid industrial email address'),
+
         password: z.string().min(8, 'Password must b
     })
 });
