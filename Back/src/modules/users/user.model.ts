cdb4fa9504594cbf7d4d22cbe355421bd26bb8d9
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-03 22:03:33 -0500

    Update user.model.ts

--- Back/src/modules/users/user.model.ts
+++ Back/src/modules/users/user.model.ts
@@ -5,10 +5,6 @@ import { IUser } from '../auth/auth.interfaces.js';
 const userSchema = new Schema<IUser>(
     {
         name: { type: String, required: true, trim: true },
-        email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-        password: { type: String, required: true, select: false }, // Oculto por defecto en consultas
-        role: {
-            type: String,
-   
+        ema
 
 
