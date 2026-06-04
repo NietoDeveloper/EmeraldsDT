fdee7cff7d8bc8377276635d31ef2769757d558f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 15:53:22 -0500

    Update auth.controller.ts

--- Back/src/modules/auth/auth.controller.ts
+++ Back/src/modules/auth/auth.controller.ts
@@ -1,25 +1,2 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { User } from '../users/user.model.js';
-import { RegisterInput, LoginInput } from '../users/user.schema.js';
-
-/**
- * 🛰️ UTILITY - ATOMIC TOKEN GENERATOR
- * Emite firmas criptográficas asimétricas de corta duración para mitigar secuestros de sesión.
- */
-const generateTokens = (uid: string, role: string) => {
-    const secret = process.env.JWT_SECRET || 'ALPHA_CLUSTER_SECRET_KEY';
-    const accessToken = jwt.sign({ uid, role }, secret, { expiresIn: '15m' }); // 15 Minutos de ventana de vida L6
-    return { accessToken };
-};
-
-/**
- * 🟢 PIPELINE - IDENTITY REGISTRATION
- * Inserta un nuevo registro de identidad en el Cluster Omega.
- */
-export const register = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        // Envoltura tipada estricta desde el validador Zod Shield
-        const { name, email, password, role } = req.body as RegisterInput;
-        
-        const exisse, next: NextFunction) => {
