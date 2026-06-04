988a9f041f165d2e4687bac1252b9201cfb3a61a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 15:46:43 -0500

    Update auth.controller.ts

--- Back/src/modules/auth/auth.controller.ts
+++ Back/src/modules/auth/auth.controller.ts
@@ -1,6 +1,3 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import { User } from '../users/user.model.js';
-
-const generateTokens = (uid: string, role: string) => {
-    const secret = process.env.JWT_SECRET || 'ALPHA_CLUSTER_SECRET_KEY';
+import { User } from '../users/user.m
