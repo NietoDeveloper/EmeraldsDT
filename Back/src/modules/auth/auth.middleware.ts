07c8c6d4b45e9d5ff395e296f18e673ab12db19e
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 15:02:53 -0500

    Update auth.middleware.ts

--- Back/src/modules/auth/auth.middleware.ts
+++ Back/src/modules/auth/auth.middleware.ts
@@ -2,8 +2,5 @@ import { Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { AuthenticatedRequest, IJWTPayload } from './auth.interfaces.js';
 
-// Guardián A: Autenticidad del Token
-export const requireAuth = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
-    try {
         const authHeader = req.headers.authorization;
   
