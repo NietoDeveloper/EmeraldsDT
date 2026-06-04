3dd7dd845f31b7264ba7dba2d1de294ded0a3740
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 16:27:42 -0500

    Update auth.interfaces.ts

--- Back/src/modules/auth/auth.interfaces.ts
+++ Back/src/modules/auth/auth.interfaces.ts
@@ -4,12 +4,7 @@ import { Request } from 'express';
 export interface IUser extends Document {
     name: string;
     email: string;
-    password?: string;
-    role: 'SUPER_ADMIN' | 'EMPLOYEE' | 'CLIENT';
-    isActive: boolean;
-    lastLogin?: Date;
-    createdAt: Date;
-    updatedAt: Date;
+
     comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
