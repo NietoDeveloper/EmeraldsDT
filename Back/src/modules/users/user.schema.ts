c80799375b911ef037482de75931c77abbef9bdc
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 03:39:24 -0500

    Update user.schema.ts

--- Back/src/modules/users/user.schema.ts
+++ Back/src/modules/users/user.schema.ts
@@ -5,7 +5,7 @@ export const registerSchema = z.object({
         name: z.string().min(2, 'Name must be at least 2 characters long'),
         email: z.string().email('Invalid industrial email address'),
         password: z.string().min(8, 'Password must be minimum 8 characters and include high complexity'),
-        role: z.enum(['SUPER_ADMIN', 'EMPLOYEE', 'CLIENT']).optional()
+
     })
 });
 
