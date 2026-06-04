9525050b48735e277b4469dfd47e67d5c4b1e574
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-03 21:19:55 -0500

    Update auth.routes.ts

--- Back/src/modules/auth/auth.routes.ts
+++ Back/src/modules/auth/auth.routes.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import { login, register } from './auth.controller.js';
 import { validate } from '../../shared/middlewares/validate.middleware.js';
 router.post('/register', validate(registerSchema), register);
-router.post('/login
+router.po
 
 export default router;
