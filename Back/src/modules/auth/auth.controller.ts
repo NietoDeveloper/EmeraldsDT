e13a27f4c920faa515232462ef1f0f0afaf4378c
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-04 15:46:37 -0500

    Update auth.controller.ts

--- Back/src/modules/auth/auth.controller.ts
+++ Back/src/modules/auth/auth.controller.ts
@@ -4,18 +4,3 @@ import { User } from '../users/user.model.js';
 
 const generateTokens = (uid: string, role: string) => {
     const secret = process.env.JWT_SECRET || 'ALPHA_CLUSTER_SECRET_KEY';
-    const accessToken = jwt.sign({ uid, role }, secret, { expiresIn: '15m' }); // Token de corta duración
-    return { accessToken };
-};
-
-export const register = async (req: Request, res: Response) => {
-    try {
-        const { name, email, password, role } = req.body;
-        
-        const existing = await User.findOne({ email });
-        if (existing) return res.status(400).json({ status: 'BAD_REQUEST', message: 'Identity already registered' });
-
-        const newUser = new User({ name, email, password, role });
-        await newUser.save();
-
-       
