e55fd5b3b353739c2b152676049f6ab0c0e544d1
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-17 17:23:52 -0500

    Update page.tsx

--- Front/src/app/[lang]/collections/page.tsx
+++ Front/src/app/[lang]/collections/page.tsx
@@ -1,8 +1,4 @@
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { apiClient } from '@/lib/apiClient';
-
-// Interfaz para el tipado estricto de la Esmeralda (
-    </main>
-  );
 }
