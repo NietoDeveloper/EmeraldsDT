1e3bae23a46100d49f1a2904e7a44660135d61f3
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-16 23:55:35 -0500

    Update apiClient.ts

--- Front/src/lib/apiClient.ts
+++ Front/src/lib/apiClient.ts
@@ -3,6 +3,6 @@
  * Diseñado bajo el estándar Software DT para comunicaciones atómicas con el clúster back-end.
  */
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api/v1';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://loca
 
 
