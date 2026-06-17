030f87325674f30517990ef9e5e6f70268e7a305
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-16 23:59:39 -0500

    Update page.tsx

--- Front/src/app/[lang]/page.tsx
+++ Front/src/app/[lang]/page.tsx
@@ -1,5 +1,1 @@
 import { HeroSection } from '@/components/sections/HeroSection';
-import { MainButton } from '@/components/ui/MainButton';
-import { Footer } from "@/components/shared/Footer"; // Importamos el Footer aquí
-
-interface HomePageProps {
