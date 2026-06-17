a552275c803f801633116bb77121751c640b742f
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-16 23:59:31 -0500

    Update page.tsx

--- Front/src/app/[lang]/page.tsx
+++ Front/src/app/[lang]/page.tsx
@@ -3,9 +3,3 @@ import { MainButton } from '@/components/ui/MainButton';
 import { Footer } from "@/components/shared/Footer"; // Importamos el Footer aquí
 
 interface HomePageProps {
-  params: Promise<{ lang: string }>;
-}
-  return (
-    <main className="fixed inset-0 w-full bg-black snap-y snap-mandatory overflow-y-auto scrollbar-hide selection:bg-gold/30 z-0">
-      
-}
