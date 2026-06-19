80c594164f195528724b3b1a2016898d13405809
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-18 23:37:02 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -1,10 +1,3 @@
 "use client";
 
-import { useScrollDirection } from "@/hooks/useScrollDirection";
-import Link from "next/link";
-import Image from "next/image";
-import { useState, useEffect } from "react";
-import { ShoppingCart, ChevronDown } from "lucide-react";
-
-interface NavSubItem {
-
+import
