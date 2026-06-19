63c18f5e4716467cfcb392306413889bbef39eef
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-18 21:45:27 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -115,11 +115,7 @@ export const Navbar = () => {
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
               className="lg:hidden relative z-[200] w-11 h-11 flex flex-col items-center justify-center bg-emerald-500/5 rounded-full border border-emerald-500/20 transition-all duration-500 hover:border-gold outline-none"
             >
-              <div className="relative w-6 h-5 flex flex-col justify-between items-center transition-all duration-500">
-                <span className={`w-full h-[2px] transition-all duration-500 ease-in-out transform ${isMobileMenuOpen ? 'rotate-45 translate-y-[9px] bg-gold' : 'bg-emerald-500'}`} />
-                <span className={`w-full h-[2px] transition-all duration-500 ease-in-out ${isMobileMenuOpen ? 'opacity-0' : 'bg-emerald-500'}`} />
-                <span className={n-out transform ${isMobileMenuOpen ? '-rotate-45 -translate-y-[9px] bg-gold' : 'bg-emerald-500'}`} />
-              </div>
+
             </button>
           </div>
         </div>
