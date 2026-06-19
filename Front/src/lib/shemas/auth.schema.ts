139f8ca908b023c91bd817d25873dfcad5ae0afa
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-19 15:16:49 -0500

    Update auth.schema.ts

--- Front/src/lib/shemas/auth.schema.ts
+++ Front/src/lib/shemas/auth.schema.ts
@@ -7,7 +7,7 @@ import { z } from 'zod';
 export const loginSchema = z.object({
   email: z
     .string()
-    .min(1, { message: 'Email is required // Correo requerido' })
+    .min(1, { message: 'Email is required // Correequerido' })
     .email({ message: 'Invalid email address // Correo inválido' })
     .toLowerCase(),
   password: z
