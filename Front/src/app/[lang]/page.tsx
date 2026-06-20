514547e0281e65fbf2d5c36ab35217c4c61baa0b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-19 20:28:29 -0500

    Update page.tsx

--- Front/src/app/[lang]/page.tsx
+++ Front/src/app/[lang]/page.tsx
@@ -132,9 +132,7 @@ export default function AuthPage({ params }: AuthPageProps) {
         </form>
       </div>
 
-      {/* Identificador de pie de página técnico discreto */}
-      <div className="absolute bottom-4 left-4 hidden md:block z-10 text-[9px] font-mono text-white/20 tracking-widest">
-        EMERALD DT // INTEGRATION
+
       </div>
     </main>
   );
