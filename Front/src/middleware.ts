428436b12a3432b9829ad3e23819e9f718a572f2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-20 03:00:08 -0500

    Update middleware.ts

--- Front/src/middleware.ts
+++ Front/src/middleware.ts
@@ -1,8 +1,2 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  // 1. Si el usuario entra a la raíz '/', enviarlo a '/es' o '/en'
-  if (pathname === '/') {
