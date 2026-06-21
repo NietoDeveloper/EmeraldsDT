f4b08064111950800f9e79e146ab12bb2f0ac51d
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-20 22:32:37 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -7,9 +7,6 @@ import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import { ShoppingCart } from "lucide-react";
 
-interface NavLink {
-  // Captura el idioma dinámico actual (por defecto 'es' para evitar fallos de SSR)
-  const lang = (params?.lang as string) || "es";
   const isEs = lang === "es";
               </span>
             </div>
