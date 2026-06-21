836e22f8003b6a097d124988a11a026656dadd0a
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-20 22:37:48 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -5,11 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
-import { ShoppingCart } from "lucide-react";
-
-  const isEs = lang === "es";
-              </span>
-            </div>
+import { ShoppingCa
           </Link>
   );
 };
