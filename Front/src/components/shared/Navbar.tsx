c48a6935b5a67b498b0cc9d45a9d4994150f6b16
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-20 22:38:17 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { useScrollDirection } from "@/hooks/useScrollDirection";
-import Link from "next/link";
-import Image from "next/image";
-import { useState, useEffect } from "react";
+
 m "next/navigation";
 import { ShoppingCa
           </Link>
