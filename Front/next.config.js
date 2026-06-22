ef1a48ba5346cc83c170dc70a7fa02a6483fdde9
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-22 02:26:04 -0500

    Update next.config.js

--- Front/next.config.js
+++ Front/next.config.js
@@ -1,27 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 1. Desbloqueo perimetral directo de WebSockets exigido por Next.js 16
+  // 1. Desbloqueo perimetral directo de WebSockets exigido por el compilador
   allowedDevOrigins: [
-    'localhost:3000',
-    '192.168.0.21:3000'
+    '192.168.0.21:3000',
+    'localhost:3000'
   ],
 
-  // Optimización avanzada de imágenes (Gemas de alta pureza)
+  // Optimización avanzada de imágenes
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**.amazonaws.com', // Conexión blindada para S3 en AWS
+        hostname: '**.amazonaws.com',
       },
     ],
   },
 
-  // Máxima seguridad y estabilidad del clúster
+  // Estabilidad del clúster
   reactStrictMode: true,
-  poweredByHeader: false, // Perímetro seguro: Mitiga escaneos de firma de framework
+  poweredByHeader: false,
 
-  // Configuración de empaquetado para entornos distribuidos
+  // Empaquetado distribuido
   output: 'standalone'
 };
 
