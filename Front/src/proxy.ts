df451be54d7c8619a5909bdd6cbf0cdb220337a3
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-22 02:27:36 -0500

    Update proxy.ts

--- Front/src/proxy.ts
+++ Front/src/proxy.ts
@@ -1,15 +1,2 @@
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-// Next.js 16 requiere que la función se llame 'proxy' si usas la convención proxy.ts
-export function proxy(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-
-    /*
-     * Excluye rutas del sistema y archivos estáticos. 
-     * El patrón `.*\\..*$` evita que el proxy evalúe imágenes de minas u otros recursos dinámicos.
-     */
-    '/((?!api|_next/static|_next/image|assets|favicon.ico|icon.png|.*\\..*$).*)',
-  ],
-};
+import type 
