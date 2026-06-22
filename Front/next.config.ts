7845590bbe6626434d2656ee84ca5afc59c37a73
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-21 23:48:00 -0500

    Update next.config.ts

--- Front/next.config.ts
+++ Front/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 /**
  * 🚀 EMERALD DT - CORE INFRASTRUCTURE CONFIG
- * Configuración optimizada para despliegues deel Nieto Laboratory.
+
  */
 const nextConfig: NextConfig = {
     'localhost:3000',  ],
