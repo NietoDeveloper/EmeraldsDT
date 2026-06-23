b317419a4d55c0c0cd303ac15705893fc9c56813
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-23 02:32:00 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -4,15 +4,3 @@ import { useScrollDirection } from "@/hooks/useScrollDirection";
 import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { useParams, useRouter } from "next/navigation"; // 1. Inyección de useRouter nativo
-import { ShoppingCart } from "lucide-react";
-
-interface NavLink {
-  name: string;
-  href: string;
-}
-
-export const Navbar = () => {
-  const scrollDirection = useScrollDirection();
-  const params = useParams();
-  const router = useRouter(); // 2. Instancia del enrutador de Next.js
