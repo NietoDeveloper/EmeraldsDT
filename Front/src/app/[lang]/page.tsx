adff3843719045b740055d4f486183074b4c90df
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-23 17:13:33 -0500

    Update page.tsx

--- Front/src/app/[lang]/page.tsx
+++
@@ -1,5 +0,0 @@
-ine, index) => (
-        <section 
-          key={mine.id}
-          className="snap-start h-screen w-full flex items-end pb-20 md:pb-32 overflow-hidden relative border-t border-white/5 shrink-0"
-        >
