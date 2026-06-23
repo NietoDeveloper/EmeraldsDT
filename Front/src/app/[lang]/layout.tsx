8589b6adcbfebd57b0a29f9b2675d1b060eb5632
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-23 00:35:34 -0500

    Update layout.tsx

--- Front/src/app/[lang]/layout.tsx
+++ Front/src/app/[lang]/layout.tsx
@@ -1,18 +1,3 @@
-import type { Metadata, Viewport } from "next";
-import { Inter, JetBrains_Mono } from "next/font/google";
-import "@/app/globals.css";
-import { Navbar } from "@/components/shared/Navbar";
-import Preloader from "@/components/shared/Preloader";
-import { Suspense } from "react";
-import PageTransitionWrapper from "@/components/shared/PageTransitionWrapper"; 
-
-const sans = Inter({
-  variable: "--font-sans",
-  subset
-  maximumScale: 5,
-  themeColor: "#000000",
-
-  params: Promise<{ lang?: string }>; 
 
   );
 }
