eb941a87a546fc7914f822c74cbf65ea3384dda2
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-23 02:32:06 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-
-import { useScrollDirection } from "@/hooks/useScrollDirection";
-import Link from "next/link";
-import Image from "next/image";
-import { useState, useEffect } from "react";
