23d4a2c4a4c577203b60b585c4aba1f98df562f8
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-24 00:43:29 -0500

    Update Preloader.tsx

--- Front/src/components/shared/Preloader.tsx
+++ Front/src/components/shared/Preloader.tsx
@@ -11,23 +11,7 @@ export default function Preloader() {
 
   // Función unificada y segura de desbloqueo global del DOM
   const releaseDOM = () => {
-    if (typeof document !== 'undefined') {
-      document.documentElement.classList.remove("js-loading");
-      document.documentElement.classList.add("js-loaded");
-    }
-  };
 
-  useEffect(() => {
-    setIsMounted(true);
-    
-    // Inicialización perimetral de bloqueo
-    if (typeof document !== 'undefined') {
-      document.documentElement.classList.add("js-loading");
-      document.documentElement.classList.remove("js-loaded");
-    }
-
-    // Ciclo total de 5 segundos de carga del Security Cluster
-    const timer = setTimeout(() => {
       setLoading(false);
     }, 5000);
 
