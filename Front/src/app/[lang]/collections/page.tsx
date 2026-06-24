8408108e1a4daec2cf91fc7aca6247521a6589df
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-24 02:37:12 -0500

    Update page.tsx

--- Front/src/app/[lang]/collections/page.tsx
+++
@@ -1,8 +0,0 @@
-ineNames[mine as keyof typeof mineNames] || mine}` : (isEs ? 'JOYAS EXHIBIDAS' : 'DISPLAYED STOCK')}
-            </h1>
-          </div>
-
-          {/* Filtros de Navegación por Mina */}
-          <div className="flex flex-wrap gap-2 font-mono text-[9px] tracking-widest font-bold w-full md:w-auto">
-       
-}
