8baadac3013d70e075e0462889201ac66de89dde
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-06-24 01:35:24 -0500

    Update Navbar.tsx

--- Front/src/components/shared/Navbar.tsx
+++ Front/src/components/shared/Navbar.tsx
@@ -145,25 +145,7 @@ export const Navbar = () => {
           <div className="flex flex-col items-center gap-6 w-full text-center mt-8">
             {navLinks.map((item) => (
               <Link 
-                key={item.href} 
-                href={item.href} 
-                onClick={() => setIsMobileMenuOpen(false)}
-                className="text-md uppercase tracking-[0.25em] font-bold text-gold/90 hover:text-white transition-colors cursor-pointer py-1 block"
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-
-          {/* Sección de Accesos Inferiores (Carrito y Acceso / Log in) */}
-          <div className="flex flex-col items-center gap-5 w-full max-w-[240px] border-t border-gold/20 pt-6 mb-4">
-            <Link 
-              href={`/${lang}/collections`} 
-              onClick={() => setIsMobileMenuOpen(false)} 
-              className="text-gold hover:text-white transition-colors flex items-center gap-3 text-xs uppercase tracking-[0.2em] font-bold cursor-pointer"
-
-            </Link>
-          </div>
+             
 
         </div>
       </div>
